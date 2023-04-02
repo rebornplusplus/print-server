985b60339649723c37c3571ebf105d6fd82d677a
Rafid Bin Mostofa <rebornplusplus@gmail.com>
2023-04-02 10:50:57 +0600

    feat(admin): Add users in bulk
    Create many users with username, password, name from the admin site.
    Available at ``/admin/accounts/siteuser/add-users/``.
    Notice the ``Add Users in Bulk`` button in ``Users`` dashboard in
    admin site.

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,70 @@
-from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+import csv
 
+from django.contrib import admin, messages
+from django.contrib.auth.admin import UserAdmin
+from django.shortcuts import render, redirect, reverse
+from django.urls.conf import path
+
+from .forms import AddMultipleUsersForm
 from .models import SiteUser
 
 
 @admin.register(SiteUser)
 class SiteUserAdmin(UserAdmin):
-    pass
+    add_users_form = AddMultipleUsersForm
+
+    def get_urls(self):
+        urls = [
+                   path(
+                       "add-users/",
+                       self.admin_site.admin_view(self.add_users_view),
+                       name="accounts_siteuser_add_users",
+                   )
+               ] + super().get_urls()
+        return urls
+
+    def add_users_view(self, request, form_url=""):
+        if request.method == "POST":
+            form = self.add_users_form(request.POST)
+            if form.is_valid():
+                added_users = []
+                failed_users = []
+                user_info_data = form.cleaned_data["users_details"]
+                user_info_list = list(csv.reader(user_info_data.split('\n'), delimiter=","))
+                for user_info in user_info_list:
+                    try:
+                        SiteUser.objects.create_user(
+                            username=user_info[0],
+                            password=user_info[1],
+                            first_name=user_info[2]
+                        )
+                    except Exception:
+                        failed_users.append(user_info[0])
+                        continue
+                    added_users.append(user_info[0])
+
+                if len(added_users) > 0:
+                    messages.success(
+                        request,
+                        "Successfully added " + str(len(added_users)) + " user(s): " + ", ".join(added_users) + "."
+                    )
+                if len(failed_users) > 0:
+                    messages.error(
+                        request,
+                        "Failed to add " + str(len(failed_users)) + " user(s): " + ", ".join(failed_users) + "."
+                    )
+
+                return redirect(reverse("admin:accounts_siteuser_changelist"))
+            else:
+                messages.error(request, "Failed to add the users.")
+        else:
+            form = self.add_users_form()
+
+        context = {
+            "title": "Add Users",
+            "form_url": form_url,
+            "form": form,
+            **self.admin_site.each_context(request),
+        }
+
+        return render(request, "admin/accounts/add_users.html", context=context)
---
+++ accounts/forms.py
@@ -0,0 +1,66 @@
+import csv
+
+from django import forms
+from django.contrib.auth.password_validation import MinimumLengthValidator, NumericPasswordValidator
+from django.contrib.auth.validators import ASCIIUsernameValidator
+from django.core.exceptions import ValidationError
+
+
+class AddMultipleUsersForm(forms.Form):
+
+    class DummyUser:
+        def __init__(self, record):
+            if len(record) != 3:
+                raise ValidationError("Invalid record: " + ",".join(record))
+            self.username = record[0]
+            self.password = record[1]
+            self.first_name = record[2]
+            self.validate()
+
+        def validate(self):
+            self.validate_username()
+            self.validate_password()
+            self.validate_first_name()
+
+        def validate_username(self):
+            ASCIIUsernameValidator().__call__(self.username)
+
+        def validate_password(self):
+            MinimumLengthValidator().validate(self.password)
+            NumericPasswordValidator().validate(self.password)
+
+        def validate_first_name(self):
+            if self.first_name is None or self.first_name == "":
+                raise ValidationError("Name cannot be empty")
+
+    users_details = forms.CharField(
+        label="Users list (CSV)",
+        widget=forms.Textarea(
+            attrs={
+                "class": "form-control",
+                "autofocus": True,
+                "style": "display: block;"
+                         "width: 70%;"
+                         "max-width: 100%"
+                         "-webkit-box-sizing: border-box;"
+                         "-moz-box-sizing: border-box;"
+                         "box-sizing: border-box;",
+                "placeholder": "username,password,name\n"
+                               "...",
+            }
+        ),
+        help_text="<br/>".join([
+            str(ASCIIUsernameValidator().message),
+            MinimumLengthValidator().get_help_text(),
+            NumericPasswordValidator().get_help_text(),
+        ]),
+    )
+
+    def clean_users_details(self):
+        data = self.cleaned_data["users_details"]
+        # check if the attributes are valid
+        # schema: username, password, first_name
+        user_info_list = list(csv.reader(data.split('\n'), delimiter=","))
+        for user_info in user_info_list:
+            self.DummyUser(user_info)
+        return data
